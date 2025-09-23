2eeb4799064f0a46ed972148fe43ff8ab4fbbfe4
Prashant5ingh <primeprashant38881@gmail.com>
2025-09-24 01:45:50 +0530

    Button css updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -58,7 +58,7 @@ function Header() {
                   {/* useNavigate (pass url to navigate) works same as Link both are from react router */}
                   <button
                     onClick={() => navigate(item.slug)}
-                    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full cursor-pointer'
                   >{item.name}</button>
                 </li>
               ) : null
